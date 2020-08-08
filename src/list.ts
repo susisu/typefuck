c8e3fee5ca00f1af7357d60874630eb71ec00164
Susisu <susisu2413@yahoo.co.jp>
2020-08-08 15:03:41 +0900

    refactor: use bare array types to represent lists

--- src/list.ts
+++ src/list.ts
@@ -1,7 +1,5 @@
 import { Recurse } from "./util";
 
-export type Nil = [];
-
 export type Cons<X, XS> = XS extends unknown[] ? [X, ...XS] : never;
 
 export type Head<XS> = XS extends [infer Y, ...infer _YS] ? Y : never;
@@ -11,6 +9,6 @@ export type Tail<XS> = XS extends [infer _Y, ...infer YS] ? YS : never;
 export type Reverse<XS> = XS extends unknown[] ? Recurse<ReverseSub<XS, []>> : never;
 
 type ReverseSub<XS extends unknown[], YS extends unknown[]> =
-    XS extends Nil ? YS
+    XS extends [] ? YS
   : XS extends [infer Z, ...infer ZS] ? { __rec: ReverseSub<ZS, [Z, ...YS]> }
   : never;
--- src/memory.ts
+++ src/memory.ts
@@ -1,4 +1,4 @@
-import { Nil, Cons, Head, Tail } from "./list";
+import { Cons, Head, Tail } from "./list";
 
 export type Memory<L, H, R,> = {
   left: L,
@@ -11,13 +11,13 @@ export type Read<M> = M extends Memory<infer _L, infer H, infer _R> ? H : never;
 export type Write<M, B> = M extends Memory<infer L, infer _H, infer R> ? Memory<L, B, R> : never;
 
 export type MoveL<M> = M extends Memory<infer L, infer H, infer R> ? (
-  L extends Nil
-    ? Memory<Nil, 0x0, Cons<H, R>>
+  L extends []
+    ? Memory<[], 0x0, Cons<H, R>>
     : Memory<Tail<L>, Head<L>, Cons<H, R>>
 ) : never;
 
 export type MoveR<M> = M extends Memory<infer L, infer H, infer R> ? (
-  R extends Nil
-    ? Memory<Cons<H, L>, 0x0, Nil>
+  R extends []
+    ? Memory<Cons<H, L>, 0x0, []>
     : Memory<Cons<H, L>, Head<R>, Tail<R>>
 ) : never;
