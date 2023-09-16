b5e0a514097788717cea170624110bee660ef8a9
Susisu <susisu2413@yahoo.co.jp>
2023-09-16 19:27:02 +0900

    chore: migrate to eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { config, map } = require("@susisu/eslint-config");
+const eslintConfigPrettier = require("eslint-config-prettier");
+const globals = require("globals");
+
+module.exports = [
+  ...map(
+    {
+      files: ["**/*.ts"],
+    },
+    [
+      config.tsTypeChecked,
+      eslintConfigPrettier,
+      {
+        languageOptions: {
+          ecmaVersion: 2022,
+          sourceType: "module",
+          parserOptions: {
+            project: "./tsconfig.json",
+          },
+          globals: globals.es2021,
+        },
+        rules: {
+          "max-len": ["warn", 120],
+          "@typescript-eslint/naming-convention": "off",
+          "@typescript-eslint/no-unused-vars": ["error", { varsIgnorePattern: "^_" }],
+        },
+      },
+    ]
+  ),
+  ...map(
+    {
+      files: ["**/*.js"],
+    },
+    [
+      config.js,
+      eslintConfigPrettier,
+      {
+        languageOptions: {
+          ecmaVersion: 2022,
+          sourceType: "commonjs",
+          globals: {
+            ...globals.es2021,
+            ...globals.node,
+          },
+        },
+      },
+    ]
+  ),
+];
