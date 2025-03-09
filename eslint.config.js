52ab9ce05284913afe801b089da7e8b77b012e88
Susisu <susisu2413@yahoo.co.jp>
2025-03-09 19:05:17 +0900

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,63 @@
 import { config } from "@susisu/eslint-config";
 import globals from "globals";
 
-export default config({ tsconfigRootDir: import.meta.dirname }, [
+export default config(
+  {
+    tsconfigRootDir: import.meta.dirname,
+  },
   {
     files: ["src/**/*.ts"],
     rules: {
       "@typescript-eslint/naming-convention": [
-        // a better exention mechanism is needed
-        ...config.tsTypeChecked().rules["@typescript-eslint/naming-convention"],
+        "warn",
+        // base (from @susisu/eslint-config)
+        {
+          selector: "variableLike",
+          // allow PascalCase as it is commonly used for classes and React components
+          format: ["camelCase", "PascalCase"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        {
+          selector: "variable",
+          // allow UPPER_CASE as it is commonly used for constants
+          format: ["camelCase", "PascalCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        {
+          selector: "memberLike",
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        {
+          // classProperty and classMethod are excluded because they will not be affected by external systems or packages
+          selector: ["objectLiteralProperty", "typeProperty", "objectLiteralMethod", "typeMethod"],
+          // allow any common format because property and method names could be defined by external systems or packages
+          format: ["camelCase", "PascalCase", "snake_case", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        {
+          selector: ["objectLiteralProperty", "typeProperty", "objectLiteralMethod", "typeMethod"],
+          modifiers: ["requiresQuotes"],
+          format: null,
+        },
+        {
+          selector: "import",
+          // allow any common format because import names could be defined by external packages
+          format: ["camelCase", "PascalCase", "snake_case", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        {
+          selector: "typeLike",
+          format: ["PascalCase"],
+          leadingUnderscore: "allow",
+          trailingUnderscore: "allow",
+        },
+        // extension
         {
           selector: ["typeProperty"],
           filter: { match: true, regex: "^__rec$" },
@@ -26,4 +76,4 @@ export default config({ tsconfigRootDir: import.meta.dirname }, [
       },
     },
   },
-]);
+);
